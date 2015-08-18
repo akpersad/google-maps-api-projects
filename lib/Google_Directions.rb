c3fd63688174b13521e75f2f159fb18a293ff573
Andrew Persad <akpersad@gmail.com>
2015-08-18 16:09:18 -0400

    Got JSON

--- lib/Google_Directions.rb
+++ lib/Google_Directions.rb
@@ -1,14 +1,29 @@
 require 'pry'
+require 'open-uri'
+require 'JSON'
+require 'yaml'
 
 class GoogleDirections
 
+	attr_reader :status, :doc, :json, :origin, :destination, :parameters
+
 	@@base_url = "https://maps.googleapis.com/maps/api/directions/json?"
 
-	def initialize(origin, destination)
-		@origin = origin
-		@destination = destination
+	@@default_values = {
+		:languages => :en,
+		:alternative => :false,
+		:mode => :transit
+	}
+
+	def initialize(origin, destination, options=@@default_values)
+		keys = YAML.load_file('application.yml')
+		api_key = keys['API_Key']
+		@origin = "origin=#{origin.gsub(/[\s,-.]/, "")}"
+		@destination = "destination=#{destination.gsub(/[\s,-.]/, "")}"		
+		@url = @@base_url + @origin + "&" + @destination + "&mode=transit&key=#{api_key}"
 	end
 
-
+	def json_call
+	  @url
+	end
 end
- # binding.pry
