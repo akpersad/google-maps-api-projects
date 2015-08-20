e32c164d1c1ecbc5f82cd2ca3e8b5a5a2c6e8e65
Andrew Persad <akpersad@gmail.com>
2015-08-20 09:30:54 -0400

    cleaned it up

--- app/data_fetchers/Google_Directions.rb
+++ app/data_fetchers/Google_Directions.rb
@@ -10,19 +10,15 @@ class GoogleDirections
 	@@base_url = "https://maps.googleapis.com/maps/api/directions/json?"
 
 	def initialize(origin, destination)
-		# keys = YAML.load_file('application.yml')
-		# api_key = keys['API_Key']
 		@origin = "origin=#{origin.gsub(/[\s,-.]/, "")}"
 		@destination = "destination=#{destination.gsub(/[\s,-.]/, "")}"		
-		@url = @@base_url + @origin + "&" + @destination + "&mode=transit&key="
+		@url = @@base_url + @origin + "&" + @destination + "&mode=transit&key=AIzaSyAV97YuJ8xutbhbNLwuvlToPYmHlU9owjg"
 
 		json_parse
 	end
 
 	def json_parse
-		# binding.pry
 		directions_hash = JSON.load(open(@url, {OpenSSL::SSL::VERIFY_PEER => OpenSSL::SSL::VERIFY_NONE}))
-		# binding.pry
 
 		if directions_hash['status'] == 'OK'
 			start_address = directions_hash['routes'][0]['legs'][0]['start_address']
@@ -37,10 +33,9 @@ class GoogleDirections
 			puts "\nYour trip will take #{trip_time}."
 			puts "Here are your directions: \n \n"
 
-			# binding.pry
 			directions_hash['routes'][0]['legs'][0]['steps'].each_with_index do |instructions|
 				if instructions['travel_mode'] == "TRANSIT"
-					# binding.pry
+		
 					if instructions['html_instructions'].include?("Subway")
 						puts "Ride the " + instructions['html_instructions'].gsub("Subway", "#{instructions['transit_details']['line']['short_name']} Train") + " for #{instructions['transit_details']['num_stops']} stops."
 					elsif 
@@ -49,23 +44,11 @@ class GoogleDirections
 				else
 					puts instructions['html_instructions']
 				end
-			# binding.pry
 			end
 		else
 			puts "You have entered an invalid address. Please go away."
 			sleep 2
 			Runner.new.call
-			# binding.pry	
 		end
 	end
 end
-
-# binding.pry
-# puts "Hello there!"
-# puts "Welcome to our NYC Public Transportation Google Directions API Project." 
-# puts "Please enter your where you will start your trip from."
-# 	start_trip = gets.chomp
-# puts "Thanks! Where do you plan to go?"
-# 	end_trip = gets.chomp
-#  GoogleDirections.new(start_trip, end_trip)
-#  @directions_hash
